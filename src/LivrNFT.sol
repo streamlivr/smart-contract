0bf46e7ebfeb0d73f736bb8ba4b0e461507eee65
Great Adams <kingifean@gmail.com>
2024-02-22 05:54:12 +0100

    ERC1155 contract

---
+++ src/LivrNFT.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "solmate/tokens/ERC1155.sol";
+import "solmate/auth/Owned.sol";
+
+contract LivrNFT is ERC1155, Owned {
+    
+
+  mapping(uint => string) public tokenURI;
+
+  constructor() ERC1155() Owned(msg.sender) {}
+
+  function mint(address _to, uint _id, uint _amount) external {
+    _mint(_to, _id, _amount, "");
+  }
+
+  function mintBatch(address _to, uint[] memory _ids, uint[] memory _amounts) external {
+    _batchMint(_to, _ids, _amounts, "");
+  }
+
+  function burn(uint _id, uint _amount) external {
+    _burn(msg.sender, _id, _amount);
+  }
+
+  function burnBatch(uint[] memory _ids, uint[] memory _amounts) external {
+    _batchBurn(msg.sender, _ids, _amounts);
+  }
+
+  function burnForMint(address _from, uint[] memory _burnIds, uint[] memory _burnAmounts, uint[] memory _mintIds, uint[] memory _mintAmounts) external onlyOwner {
+    _batchBurn(_from, _burnIds, _burnAmounts);
+    _batchMint(_from, _mintIds, _mintAmounts, "");
+  }
+
+  function setURI(uint _id, string memory _uri) external {
+    tokenURI[_id] = _uri;
+    emit URI(_uri, _id);
+  }
+
+  function uri(uint _id) public override view returns (string memory) {
+    return tokenURI[_id];
+  }
+
+  function changeOwner(address newOwner) external onlyOwner {
+    transferOwnership(newOwner);
+  }
+
+}
