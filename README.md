ff584b791ff8d24d6ec2df18a3881c2fe76e3da0
Great Adams <kingifean@gmail.com>
2024-08-22 19:50:51 +0100

    Nft contract

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
+# Streamlivr smart contracts
+This code base consists of 5 contracts: LivrNft, LivrTicket, LivrSale, LivrToken, Staking.sol
+
+## LivrToken.sol
+Is our Livr Token contract which is an [ERC20](https://eips.ethereum.org/EIPS/eip-20) and has functions of one.
+
+## LivrNft
+This contract is an [ERC1155](https://eips.ethereum.org/EIPS/eip-1155) nft contract.
+The Nft standard used is the ERC1155 nft standard to manage total supply of nfts and tokens.
+### Variable
+- name: Livr Nft
+- symbol: LN
+- s_tokenURI: is a maaping that stores uri for every token
+
+### Functions
+- mint: mint funtion.
+  <u>Params</u>
+  - address _to: the address the nft would be mint to
+  - uint256 _id: id of the token
+  - uint256 _amount: total supply of the nft
+- mintBatch: mint more than one.
+  <u>Params</u>
+    - address _to: address the nft would be mint to
+    - uint256[] _id: array of ids for the token
+    - uint256[] _amount: array of total supply of the tokenId
+- burn: burn a token
+  <u>Params</u>
+    - uint256 _id: id of the token
+    - uint256 _amount: total supply of the nft
+- burnBatch: burn more than one token.
+  <u>Params</u>
+    - uint256[] _id: id of the token
+    - uint256[] _amount: total supply of the nft
+- setURI: set token uri.
+  <u>Params</u>
+    - uint256 _id: id of the token
+    - string _uri: url for the nft
+- changeOwner: change owner of contract.
+  <u>Params</u>
+      - address newOwner: address of the new owner.
+- uri: gets the uri of the tokenId passed to it.
+  <u>Params</u>
+      - uint256 _id_: id of token.
 
 
-Transactions with the Livr tokens we take a commision or fee
+
 
 # Staking Periods and reward percentage
 - 30 days 1.5%
---
+++ src/LivrNft.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/interfaces/IERC1155.sol";
+
+contract Lazy is ERC1155, ReentrancyGuard, Ownable, ERC1155Pausable, ERC1155Supply {
+    /////////////////////
+    // State variables //
+    /////////////////////
+
+    string public constant name = "Livr NFT";
+    string public constant symbol = "LN";
+
+    mapping(uint256 => string) public s_tokenURI;
+
+    /////////////////////
+    // Functions       //
+    /////////////////////
+
+    constructor() ERC1155("") Ownable(msg.sender) {}
+
+    ////////////////////////
+    // External Functions //
+    ///////////////////////
+
+    function mint(address _to, uint256 _id, uint256 _amount) external {
+        _mint(_to, _id, _amount, "");
+    }
+
+    function mintBatch(address _to, uint256[] memory _ids, uint256[] memory _amounts) external onlyOwner {
+        _mintBatch(_to, _ids, _amounts, "");
+    }
+
+    function burn(uint256 _id, uint256 _amount) external onlyOwner {
+        _burn(msg.sender, _id, _amount);
+    }
+
+    function burnBatch(uint256[] memory _ids, uint256[] memory _amounts) external onlyOwner {
+        _burnBatch(msg.sender, _ids, _amounts);
+    }
+
+    function burnForMint(
+        address _from,
+        uint256[] memory _burnIds,
+        uint256[] memory _burnAmounts,
+        uint256[] memory _mintIds,
+        uint256[] memory _mintAmounts
+    ) external onlyOwner {
+        _burnBatch(_from, _burnIds, _burnAmounts);
+        _mintBatch(_from, _mintIds, _mintAmounts, "");
+    }
+
+    function setURI(uint256 _id, string memory _uri) external {
+        s_tokenURI[_id] = _uri;
+        emit URI(_uri, _id);
+    }
+
+    function changeOwner(address newOwner) external onlyOwner {
+        transferOwnership(newOwner);
+    }
+
+    ////////////////////////
+    // Public Functions //
+    ///////////////////////
+
+    function uri(uint256 _id) public view override returns (string memory) {
+        return s_tokenURI[_id];
+    }
+
+    ////////////////////////
+    // Internal Functions //
+    ///////////////////////
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+        internal
+        override(ERC1155, ERC1155Pausable, ERC1155Supply)
+    {
+        super._update(from, to, ids, values);
+    }
+}
